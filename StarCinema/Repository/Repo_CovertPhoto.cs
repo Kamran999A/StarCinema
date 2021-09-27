e8127e1148bce1e9e147c7923d11a4bed6a099fa
Kamran999A <85412897+Kamran999A@users.noreply.github.com>
2021-09-27 23:11:55 +0400

    Add project files.

---
+++ StarCinema/Repository/Repo_CovertPhoto.cs
@@ -0,0 +1,23 @@
+﻿using StarCinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarCinema.Repo
+{
+    public class Repo_CovertPhoto
+    {
+        public List<ImagesforCover> GetAll() {
+            return new List<ImagesforCover>
+            { 
+                       new ImagesforCover
+                       {
+                         FilePath1 = "../../Images/Cover Images/stargif.gif",
+                         FilePath2 = "../../Images/CMS.png",
+                       },
+            };
+        }
+    }
+}
